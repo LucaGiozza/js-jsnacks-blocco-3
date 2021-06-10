2ca609f8cc5876fe12e6447d2114eef5a8acd75e
Luca Giozza <luca.giozza@gmail.com>
2021-06-10 15:41:45 +0200

    quarto esercizio

--- js/script.js
+++ js/script.js
@@ -5,24 +5,24 @@
 // compresa tra i due numeri
 
 
-// var numeri = [1, 30, 56, 67, 120, 150];
+ var numeri = [1, 30, 56, 67, 120, 150];
 
-// var x = parseInt(prompt('inserisci un numero minimo 1 e massimo ' + numeri.length));
-// var y = parseInt(prompt('inserisci un numero massimo ' + numeri.length));
+ var x = parseInt(prompt('inserisci un numero minimo 1 e massimo ' + numeri.length));
+ var y = parseInt(prompt('inserisci un numero massimo ' + numeri.length));
 
 
-// valore(numeri, x, y);
+ valore(numeri, x, y);
 
-// console.log(valore(numeri, x, y));
+ console.log(valore(numeri, x, y));
 
-// function valore(array, min, max){
-//     var nuovo = [];
-//     for (var i = min - 1; i < max; i++ ){
-//         nuovo.push(array[i]);
+ function valore(array, min, max){
+     var nuovo = [];
+     for (var i = min - 1; i < max; i++ ){
+         nuovo.push(array[i]);
 
-//     }
-//   return nuovo;
-// }
+     }
+   return nuovo;
+ }
 
 
 
@@ -59,10 +59,44 @@ function lunghezza(x, y){
 
 
 
-
+// terzo esercizio
 // 3.  Scrivi una funzione che fonda due array (con lo stesso numero 
 //     di elementi) prendendo alternativamente gli elementi da uno e dall’altroes.
 //      [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
+  var lettere = ['a','b','c'];
+  var numeri = [1, 2, 3];
+
+  alternanza(lettere, numeri)
+
+  function alternanza(x,y){
+    
+      for(var i = 0; i < x.length ; i++){
+        
+          console.log(x[i])
+          console.log(y[i])
+        
+      }
+
+
+
+  }
+
+
+
+
+
 // 4. Scrivi una funzione che prenda una stringa e la trasformi con l'iniziale
 //  maiuscola e 
 // tutto il resto in minuscolo.
+
+
+
+
+var inserimento = prompt('inserisci una parola')
+
+function letteraMaiuscola(parola) {
+     return parola.charAt(0).toUpperCase() + parola.slice(1);
+  }
+  
+  console.log(letteraMaiuscola(inserimento)); 
