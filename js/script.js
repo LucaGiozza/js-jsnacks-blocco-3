c30a174e3d14e7d9ad8c80fcf25e9268641624c8
Luca Giozza <luca.giozza@gmail.com>
2021-06-10 14:37:17 +0200

    secondo esercizio

--- js/script.js
+++ js/script.js
@@ -5,34 +5,61 @@
 // compresa tra i due numeri
 
 
-var numeri = [1, 30, 56, 67, 120, 150];
+// var numeri = [1, 30, 56, 67, 120, 150];
 
-var x = parseInt(prompt('inserisci un numero minimo 1 e massimo ' + numeri.length));
-var y = parseInt(prompt('inserisci un numero massimo ' + numeri.length));
-
-
-valore(numeri, x, y);
-
-console.log(valore(numeri, x, y));
-
-function valore(array, min, max){
-    var nuovo = [];
-    for (var i = min - 1; i < max; i++ ){
-        nuovo.push(array[i]);
-
-    }
-  return nuovo;
-}
+// var x = parseInt(prompt('inserisci un numero minimo 1 e massimo ' + numeri.length));
+// var y = parseInt(prompt('inserisci un numero massimo ' + numeri.length));
 
 
+// valore(numeri, x, y);
+
+// console.log(valore(numeri, x, y));
+
+// function valore(array, min, max){
+//     var nuovo = [];
+//     for (var i = min - 1; i < max; i++ ){
+//         nuovo.push(array[i]);
+
+//     }
+//   return nuovo;
+// }
 
 
 
+
+// secondo esercizio
 // Dare la possibilità di inserire due parole. 
 //        Verificare tramite una funzione che le due parole 
 //        abbiano la stessa lunghezza.    Se hanno la stessa 
 //        lunghezza, stamparle entrambe altrimenti stampare la 
 //        più lunga delle due.
+
+
+
+
+var primaParola = prompt('inserisci prima parola');
+var secondaParola = prompt('inserisci seconda parola');
+
+ var risultato = lunghezza(primaParola, secondaParola);
+ console.log(risultato);
+
+function lunghezza(x, y){
+    var ris;
+     if(x.length == y.length){
+         ris = 'sono uguali'
+     }else if(x.length > y.length){
+         ris = x
+     }else{
+        ris = y
+     }
+     return ris;
+
+ }
+ 
+
+
+
+
 // 3.  Scrivi una funzione che fonda due array (con lo stesso numero 
 //     di elementi) prendendo alternativamente gli elementi da uno e dall’altroes.
 //      [a,b,c], [1,2,3] → [a,1,b,2,c,3].
